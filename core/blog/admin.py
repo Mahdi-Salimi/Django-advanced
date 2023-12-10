3e7bbb41e359f3b4ae3da4f9b0a58b1ecb4ca4f4
Mahdi-S97 <mahdisalimi.dev@gmail.com>
2023-12-10 10:14:03 +0330

    Complete Models
    use signal to relate user and profile
    set static and media
    complete blog model

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Category,Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','category','created_date','status']
+    list_filter = ['status','created_date']
+    search_fields = ['title','content']
+    class Meta:
+        model = Post
+        
+admin.site.register(Category)
+admin.site.register(Post)
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+# getting user model object
+User = get_user_model()
 class Post(models.Model):
     '''
     this is a class for define posts for blog app
@@ -10,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     status = models.BooleanField()
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
